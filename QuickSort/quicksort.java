6df6c2f8a132e55bb00485b9471cba32deb29463
fersanz_06 <luisfernandosanchezbriseno052@gmail.com>
2025-11-19 19:40:35 -0600

    Commit 4

--- QuickSort/quicksort.java
+++ QuickSort/quicksort.java
@@ -1,10 +1,10 @@
 package QuickSort;
 import java.util.Arrays;
-public class QuickSort{
+public class QuickSort {
     public static void quickSort(int[] arr, int low, int high) {
         if (low < high) {
             int pi = partition(arr, low, high);
-            quickSort(arr, low, pi);
+            quickSort(arr, low, pi - 1);
             quickSort(arr, pi + 1, high);
         }
     }
@@ -13,7 +13,7 @@ public class QuickSort{
         int pivot = arr[high];
         int i = low - 1;
         for (int j = low; j <= high - 1; j++) {
-            if (arr[j] <= pivot) {
+            if (arr[j] >= pivot) {
                 i++;
                 swap(arr, i, j);
             }
